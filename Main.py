3759481bb9d0be4f5e8f8498ab8464189acace6b
ooflucas <lucas.chiman1@gmail.com>
2025-12-26 23:20:26 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import random
 import sys
 left_counter = 1
 right_counter = 10
-column = 10
+column = 5
 row = 5
 pos = [' '] * column * row * 2
 steps = []
@@ -18,55 +18,76 @@ def generate_mine():
         pos[mines] = '*'
         mine_pos.append(mines)
 def corners():
-    corner = []
     global column
     global row
-    corner.append(1)
-    corner.append(column)
-    corner.append(column*(row-1)+1)
-    corner.append(column*row)
-    print(corner)
-    return corner
+    top_right_corner = 1
+    top_left_corner = column
+    bottom_right_corner = (column*(row-1)+1)
+    bottom_left_corner = (column*row)
+    return top_right_corner, top_left_corner, bottom_right_corner, bottom_left_corner
 def sides():
     side_list = []
     left_side = []
     right_side = []
     bottom_side = []
     top_side = []
+    topright, topleft, bottomright, bottomleft = corners()
+    corner = [topright, topleft, bottomright, bottomleft]
     global column
     global row
-    corner = corners()
     for top in range(column):
         top += 1
         if top in corner:
             continue
         top_side.append(top)
-    print(f"top = {top_side}")
     for bottom in range(column*(row-1), column*row):
         bottom += 1
         if bottom in corner:
             continue
         bottom_side.append(bottom)
-    print(f"bottom = {bottom_side}")
-    for left in range(column):
-        left += 1
-        left += column
-        if left in corner:
+    for right in range(row):
+        right += 1
+        right = right*column
+        if right in corner:
             continue
-        left_side.append(left)
-    print(f"left = {left_side}")
+        right_side.append(right)
+    lef = 1
+    for left in range(row-1):
+        lef += column
+        if lef in corner:
+            continue
+        left_side.append(lef)
+    return top_side, bottom_side, left_side, right_side
 def find_adjacent_cells(positions):
-    offsets = [-5, 5, -1, 1, -6, -4, 4, 6]
+    top, bottom, left, right = sides()
+    top_right, top_left, bottom_right, bottom_left = corners()
+    print(f'top = {top}')
+    print(f'bottom = {bottom}')
+    print(f'left = {left}')
+    print(f'right = {right}')
+    print(f'top right = {top_right}')
+    print(f'top left = {top_left}')
+    print(f'bottom right = {bottom_right}')
+    print(f'bottom left = {bottom_left}')
+    print('\n')
     adjacent_list = []
     positions = int(positions)
-    upper = positions - 5
-    lower = positions + 5
+    if positions in top_right:
+        right = positions + 1
+        lower = positions + column
+        lower_right = positions + column
+    elif positions in top_left:
+        left = positions - 1
+        lower = positions + column
+        lower_left = positions + column - 1
+    upper = positions - column
+    lower = positions + column
     left = positions - 1
     right = positions + 1
-    upper_left = positions - 6
-    upper_right = positions - 4
-    lower_left = positions + 4
-    lower_right = positions + 6
+    upper_left = positions - column+1
+    upper_right = positions - column-1
+    lower_left = positions + column-1
+    lower_right = positions + column+1
     adjacent_list = [upper, lower, left, right, upper_left, upper_right, lower_left, lower_right]
     return adjacent_list
 def step():
