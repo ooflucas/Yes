eb21e702fef987bec18c85f82a9dc4fc6aa25e15
ooflucas <lucas.chiman1@gmail.com>
2026-01-02 09:54:58 +0800

    complete version

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,6 +1,6 @@
 import random
 import sys
-column = 5
+column = 10
 row = 5
 pos = [' '] * column * row * 2
 steps = []
@@ -22,7 +22,7 @@ def generate_mine():
     global column
     mines = random.sample(range(1, row*column), round(row*column*0.3))
     for i in range(len(mines)):
-        pos[mines[i]] = '*'
+        # pos[mines[i]] = '*'
         mine_pos.append(mines[i])
     return mine_pos
 def corners():
@@ -67,84 +67,46 @@ def sides():
         left_side.append(lef)
     return top_side, bottom_side, left_side, right_side
 def find_adjacent_cells(positions):
+    offest = []
     topside, bottomside, leftside, rightside = sides()
     top_right, top_left, bottom_right, bottom_left = corners()
-    print(f'top = {topside}')
-    print(f'bottom = {bottomside}')
-    print(f'left = {leftside}')
-    print(f'right = {rightside}')
-    print(f'top right = {top_right}')
-    print(f'top left = {top_left}')
-    print(f'bottom right = {bottom_right}')
-    print(f'bottom left = {bottom_left}')
     print('\n')
     adjacent_list = []
     positions = int(positions)
     if positions == top_right:
-        left = positions - 1
-        lower = positions + column
-        lower_left = positions + column - 1
+        left, lower, lower_left = positions - 1, positions + column, positions + column - 1
         adjacent_list = [lower, left, lower_left]
         return adjacent_list
     elif positions == top_left:
-        right = positions + 1
-        lower = positions + column
-        lower_right = positions + column + 1
+        right, lower, lower_right = positions + 1, positions + column, positions + column + 1
         adjacent_list = [right, lower, lower_right]
         return adjacent_list
     elif positions == bottom_left:
-        upper = positions - column
-        upper_right = positions - column + 1
-        right = positions + 1
+        upper, upper_right, right = positions - column, positions - column + 1, positions + 1
         adjacent_list = [upper, upper_right, right]
         return adjacent_list
     elif positions == bottom_right:
-        left = positions - 1
-        upper = positions + column
-        upper_left = positions + column - 1
+        left, upper, upper_left = positions - 1, positions - column, positions - column - 1
         adjacent_list = [left, upper, upper_left]
         return adjacent_list
     elif positions in topside:
-        lower = positions + column
-        left = positions - 1
-        right = positions + 1
-        lower_left = positions + column - 1
-        lower_right = positions + column + 1
+        lower, left, right, lower_left, lower_right = positions + column, positions - 1, positions + 1, positions + column - 1, positions + column + 1
         adjacent_list = [lower, left, right, lower_left, lower_right]
         return adjacent_list
     elif positions in bottomside:
-        upper = positions - column
-        left = positions - 1
-        right = positions + 1
-        upper_left = positions - column - 1
-        upper_right = positions - column + 1
+        upper, left, right, upper_left, upper_right = positions - column, positions - 1, positions + 1, positions - column - 1, positions - column + 1
         adjacent_list = [upper, left, right, upper_left, upper_right]
         return adjacent_list
     elif positions in leftside:
-        upper = positions - column
-        lower = positions + column
-        right = positions + 1
-        upper_right = positions - column + 1
-        lower_right = positions + column + 1
+        upper, lower, right, upper_right, lower_right = positions - column, positions + column, positions + 1, positions - column + 1, positions + column + 1
         adjacent_list = [upper, lower, right, upper_right, lower_right]
         return adjacent_list
     elif positions in rightside:
-        upper = positions - column
-        lower = positions + column
-        left = positions - 1
-        upper_left = positions - column - 1
-        lower_left = positions + column - 1
+        upper, lower, left, upper_left, lower_left = positions - column, positions + column, positions - 1, positions - column - 1, positions + column - 1
         adjacent_list = [upper, lower, left, upper_left, lower_left]
         return adjacent_list
     else:
-        upper = positions - column
-        lower = positions + column
-        left = positions - 1
-        right = positions + 1
-        upper_left = positions - column - 1
-        upper_right = positions - column + 1
-        lower_left = positions + column - 1
-        lower_right = positions + column + 1
+        upper, lower, left, right, upper_left, upper_right, lower_left, lower_right = positions - column, positions + column, positions - 1, positions + 1, positions - column - 1, positions - column + 1, positions + column - 1, positions + column + 1
         adjacent_list = [upper, lower, left, right, upper_left, upper_right, lower_left, lower_right]
         return adjacent_list
 def step():
