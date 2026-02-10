3ab142d05cfa36778d2af538decea54d7e780c5e
ooflucas <lucas.chiman1@gmail.com>
2026-02-10 23:32:12 +0800

    Refactor bus.py for new route and speed calculation

--- project/bus.py
+++ project/bus.py
@@ -1,34 +1,53 @@
 import requests
 import json
 from datetime import datetime, timezone, timedelta
-
-station = 'yl242'.upper()
+station = 'yl556'.upper()
 direction = None
-route = '68F'
+route = '76K'
 service_type = 1
-
+stop_id = None
 target_tz = timezone(timedelta(hours=8))
 current_time = datetime.fromisoformat((datetime.now(target_tz)).isoformat(timespec = 'minutes'))
-
 stop_list = ((requests.get('https://data.etabus.gov.hk/v1/transport/kmb/stop')).json()).get('data')
-
 inbound_data = (((requests.get(f'https://data.etabus.gov.hk/v1/transport/kmb/route/{route}/inbound/1')).json()).get('data')).get('route')
 outbound_data = (((requests.get(f'https://data.etabus.gov.hk/v1/transport/kmb/route/{route}/outbound/1')).json()).get('data')).get('route')
-
+json_path = r'C:\Users\Kelly\Desktop\project\stop_data.json'
+with open(json_path, 'r', encoding='utf-8') as f:
+        stop_data = json.load(f)
+def calculate_speed(stop_data: dict, route: str, target_id: str, delta: int):
+    if delta  <= 0:
+        print('bus arriving/departed')
+        return
+    route_key = f'{route}_1'
+    stops = stop_data.get(route_key, {})
+    prev_stop = None
+    for seq in sorted(stops.keys(), key=int):
+        current_stop = stops[seq]
+        current_id = current_stop.get('hex_id') 
+        if current_id == target_id:
+            if prev_stop:
+                dist_gap = round(current_stop['dist'] - prev_stop['dist'])
+                print(f'distance from last stop: {dist_gap} M')
+                dist_gap = dist_gap / 1000
+                delta = delta / 60
+                speed = dist_gap / delta
+                print(f'bus speed: {speed}')
+            else:
+                print("This is the first stop (no previous stop available).")
+            return 
+        prev_stop = current_stop
 if route in outbound_data:
     direction = 'outbound'
 elif route in inbound_data:
     direction = 'inbound'
-
 for stop in stop_list:
     if station in stop.get('name_en'):
         stop = stop
         stop_id = stop.get('stop')
         print(f'station: {stop.get('name_tc')}')
-
 eta = ((requests.get(f'https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/{stop_id}')).json()).get('data')
-
 ran = False
+first_ran = False
 for bus_eta in eta:
     if route in bus_eta.get('route') and bus_eta.get('service_type') == service_type:
         if not ran:
@@ -37,7 +56,8 @@ for bus_eta in eta:
         bus_etas = datetime.fromisoformat(bus_eta.get('eta'))
         delta = bus_etas - current_time
         eta_delta = round((delta.total_seconds())/60)
-        print(f'eta: {eta_delta} mintes')
+        print(f'\neta: {eta_delta} mintes')
+        calculate_speed(stop_data, route, stop_id, eta_delta)
         if eta_delta <= 1:
             print('Arriving\n')
         elif eta_delta < 0:
