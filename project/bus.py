5c95adb09c5d71b663b6ea9aeb641fde5b0f1f9e
ooflucas <lucas.chiman1@gmail.com>
2026-02-11 10:04:53 +0800

    Refactor bus.py to improve structure and readability

--- project/bus.py
+++ project/bus.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from pathlib import Path
 from datetime import datetime, timezone, timedelta
 station = 'yl556'.upper()
 direction = None
@@ -11,9 +12,18 @@ current_time = datetime.fromisoformat((datetime.now(target_tz)).isoformat(timesp
 stop_list = ((requests.get('https://data.etabus.gov.hk/v1/transport/kmb/stop')).json()).get('data')
 inbound_data = (((requests.get(f'https://data.etabus.gov.hk/v1/transport/kmb/route/{route}/inbound/1')).json()).get('data')).get('route')
 outbound_data = (((requests.get(f'https://data.etabus.gov.hk/v1/transport/kmb/route/{route}/outbound/1')).json()).get('data')).get('route')
-json_path = r'C:\Users\Kelly\Desktop\project\stop_data.json'
-with open(json_path, 'r', encoding='utf-8') as f:
+current_dir = Path(__file__).parent
+file_path = current_dir / "stop_data.json"
+with open(file_path, 'r', encoding='utf-8') as f:
         stop_data = json.load(f)
+def traceback(stop_data: dict, route: str, target_id: str, eta_data: dict, delta: int):
+    stop_data = stop_data.get(f'{route}_1')
+    for seq in stop_data:
+        hex_id = (stop_data.get(f'{seq}').get('hex_id'))
+        if hex_id == target_id:
+            prev_hex_id = stop_data.get(f'{int(seq) - 1}').get('hex_id')
+            print(prev_hex_id)
+            print(hex_id)
 def calculate_speed(stop_data: dict, route: str, target_id: str, delta: int):
     if delta  <= 0:
         print('bus arriving/departed')
@@ -44,14 +54,14 @@ for stop in stop_list:
     if station in stop.get('name_en'):
         stop = stop
         stop_id = stop.get('stop')
-        print(f'station: {stop.get('name_tc')}')
+        print(f"station: {stop.get('name_tc')}")
 eta = ((requests.get(f'https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/{stop_id}')).json()).get('data')
 ran = False
 first_ran = False
 for bus_eta in eta:
     if route in bus_eta.get('route') and bus_eta.get('service_type') == service_type:
         if not ran:
-            print(f'route: {bus_eta.get('route')} {bus_eta.get('dest_tc')}')
+            print(f"route: {bus_eta.get('route')} {bus_eta.get('dest_tc')}")
             ran = True
         bus_etas = datetime.fromisoformat(bus_eta.get('eta'))
         delta = bus_etas - current_time
@@ -62,4 +72,4 @@ for bus_eta in eta:
             print('Arriving\n')
         elif eta_delta < 0:
             print('Departed\n')
-
+traceback(stop_data, route, stop_id, eta, delta)
